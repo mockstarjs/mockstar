adbf484a0a855726c25cf5a7d8ad2ffab3217e12
helinjiang <onehlj@qq.com>
2018-10-19 00:17:11 +0800

    feat: 引入 eslint

--- packages/mockstar/src/index.js
+++ packages/mockstar/src/index.js
@@ -1,19 +1,19 @@
 module.exports = {
-  util: require('./util'),
-  config: require('./config'),
-  store: require('./store'),
+    util: require('./util'),
+    config: require('./config'),
+    store: require('./store'),
 
-  Case: require('./model/Case'),
-  CaseConfig: require('./model/CaseConfig'),
+    Case: require('./model/Case'),
+    CaseConfig: require('./model/CaseConfig'),
 
-  MockerParser: require('./model/MockerParser'),
+    MockerParser: require('./model/MockerParser'),
 
-  Mocker: require('./model/Mocker'),
-  MockerConfig: require('./model/MockerConfig'),
+    Mocker: require('./model/Mocker'),
+    MockerConfig: require('./model/MockerConfig'),
 
-  MockModule: require('./model/MockModule'),
-  MockModuleConfig: require('./model/MockModuleConfig'),
+    MockModule: require('./model/MockModule'),
+    MockModuleConfig: require('./model/MockModuleConfig'),
 
-  MatmanQuery: require('./model/MatmanQuery'),
-  MatmanQueryItem: require('./model/MatmanQueryItem')
-};
+    MatmanQuery: require('./model/MatmanQuery'),
+    MatmanQueryItem: require('./model/MatmanQueryItem')
+}
