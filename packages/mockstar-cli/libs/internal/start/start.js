a3cb3070126a7ed3904eeaebf02c894964d63177
linjianghe <linjianghe@tencent.com>
2018-10-22 17:32:58 +0800

    feat: 增加 -p 或者 --port 命令来启动指定的端口 #2

--- packages/mockstar-cli/libs/internal/start/start.js
+++ packages/mockstar-cli/libs/internal/start/start.js
@@ -12,6 +12,8 @@ const localServer = require('../../../biz/local-server');
  * @param {Object} args 参数
  * @param {Boolean} [args.dev] 是否为开发者模式，使用方式: --dev
  * @param {String} [args.config] 自定义配置文件，使用方式: --config=mockstar.config.js
+ * @param {Number} [args.port] 自定义服务启动端口，使用方式: --port=9527
+ * @param {Number} [args.p] 自定义服务启动端口，使用方式: -p 9527
  */
 module.exports = function (args) {
     // console.log(args);
@@ -22,7 +24,13 @@ module.exports = function (args) {
     // mockstar start --dev
     const isDev = !!args.dev;
 
+    // 自定义服务启动端口，
+    // mockstar start --port=9527
+    // mockstar start -p 9527
+    const port = args.port || args.p;
+
     // 传递进来的文件，或者默认的 mockstar.config.js 文件
+    // mockstar start --config=mockstar.config.js
     let config = args.config || 'mockstar.config.js';
 
     // 绝对路径
@@ -38,7 +46,7 @@ module.exports = function (args) {
 
     // 启动本地服务
     console.log('Ready to start local server!');
-    localServer.startServer(isDev, configAbsolutePath, cwd);
+    localServer.startServer(isDev, configAbsolutePath, cwd, { port: port });
 
     return Promise.resolve();
 };
