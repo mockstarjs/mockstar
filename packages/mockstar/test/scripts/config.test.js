0761cf5113129686d9bd6fb49cea4b4026d91411
helinjiang <onehlj@qq.com>
2018-10-23 13:45:14 +0800

    refactor: 修改为 _ms_target_ 参数来指定要的数据源

--- packages/mockstar/test/scripts/config.test.js
+++ packages/mockstar/test/scripts/config.test.js
@@ -1,14 +1,10 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const { config } = require('../../lib');
+const { MOCK_MODULES } = require('../../lib/config');
 
 describe('./config.js', () => {
-  it('should be correct', () => {
-    expect(config).to.eql({
-      MATMAN_QUERY_KEY: '_matman',
-      MOCK_MODULES: 'mock_modules',
-      TARGET_FIELD: '_m_target'
+    it('MOCK_MODULES should be correct', () => {
+        expect(MOCK_MODULES).to.equal('mock_modules');
     });
-  });
 });
