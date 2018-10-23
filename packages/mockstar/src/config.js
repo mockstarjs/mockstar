3fec4097b5bc91e885d956048af4c1e4bfc09669
linjianghe <linjianghe@tencent.com>
2018-10-23 15:06:25 +0800

    refactor: 优化 MockModuleConfig.js

--- packages/mockstar/src/config.js
+++ packages/mockstar/src/config.js
@@ -1,6 +1,13 @@
 /**
- * mock_modules 的名字
+ * mock_modules 的字段名字
  *
  * @type {String}
  */
 export const MOCK_MODULES = 'mock_modules';
+
+/**
+ * 桩数据模块用于标记唯一性的字段名字
+ *
+ * @type {String}
+ */
+export const MS_TARGET = '_ms_target_';
--- packages/mockstar/test/scripts/model/MockModuleConfig.test.js
+++ packages/mockstar/test/scripts/model/MockModuleConfig.test.js
@@ -1,51 +1,49 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const { MockModuleConfig } = require('../../../lib');
+const MockModuleConfig = require('../../../lib/model/MockModuleConfig').default;
 
-describe('./mocker/MockModuleConfig.js', () => {
-  describe('check exist-config', () => {
-    let mockModuleConfig;
+describe.only('./mocker/MockModuleConfig.js', () => {
+    describe('check exist-config', () => {
+        let mockModuleConfig;
 
-    before(() => {
-      mockModuleConfig = new MockModuleConfig('exist-config', require('../../data/fixtures/mock_modules/exist-config/config'));
-      // console.log(mockModuleConfig);
+        before(() => {
+            mockModuleConfig = new MockModuleConfig('exist-config', require('../../data/fixtures/mock_modules/exist-config/config'));
+            // console.log(mockModuleConfig);
+        });
+
+        it('should be instanceof MockModuleConfig ', () => {
+            expect(mockModuleConfig).to.be.an.instanceof(MockModuleConfig);
+        });
+
+        it('should contain some fields', () => {
+            expect(mockModuleConfig).to.have.all.keys('name', 'description', 'delay', 'match');
+        });
+
+        it('should equal correct value', () => {
+            expect(mockModuleConfig).to.eql({
+                name: 'exist-config',
+                description: 'exist-config description',
+                delay: 10086,
+                match: {
+                    _ms_target_: 'exist-config',
+                    type: 1
+                }
+            });
+        });
     });
 
-    it('should be instanceof MockModuleConfig ', () => {
-      expect(mockModuleConfig).to.be.an.instanceof(MockModuleConfig);
+    describe('check no-config', () => {
+        it('should equal correct value', () => {
+            expect(new MockModuleConfig('no-config')).to.eql({
+                name: 'no-config',
+                description: 'no-config',
+                delay: 0,
+                match: {
+                    _ms_target_: 'no-config'
+                }
+            });
+        });
     });
-
-    it('should contain some fields', () => {
-      expect(mockModuleConfig).to.have.all.keys('name', 'description', 'priority', 'delay', 'query');
-    });
-
-    it('should equal correct value', () => {
-      expect(mockModuleConfig).to.eql({
-        name: 'exist-config',
-        description: 'exist-config description',
-        priority: 88,
-        delay: 10086,
-        query: {
-          _ms_target_: 'exist-config',
-          type: 1
-        }
-      });
-    });
-  });
-
-  describe('check no-config', () => {
-    it('should equal correct value', () => {
-      expect(new MockModuleConfig('no-config')).to.eql({
-        name: 'no-config',
-        description: 'no-config',
-        priority: 0,
-        delay: 0,
-        query: {
-          _ms_target_: 'no-config'
-        }
-      });
-    });
-  });
 });
 
