30c045cf2827f58814df48633fb22cd9c1c92b8d
helinjiang <onehlj@qq.com>
2018-10-24 11:07:17 +0800

    docs: 文档更新

--- packages/mockstar-cli/libs/internal/pm2/index.js
+++ packages/mockstar-cli/libs/internal/pm2/index.js
@@ -4,5 +4,5 @@ module.exports = function (ctx) {
 
   const cmd = ctx.cmd;
 
-    cmd.register('pm2', 'Command pass to pm2', {}, require('./pm2'));
+    cmd.register('pm2', 'Commands pass to pm2', {}, require('./pm2'));
 };
