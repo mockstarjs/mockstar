7ca4dac9362e7fad87671631aa6aa4c4dad699ed
linjianghe <linjianghe@tencent.com>
2018-10-24 11:59:24 +0800

    feat:  mockstar init 支持 --dev 命令，用于强制覆盖同名文件夹

--- packages/mockstar-cli/libs/internal/init/init.js
+++ packages/mockstar-cli/libs/internal/init/init.js
@@ -10,7 +10,8 @@ const mockstarPkg = require('mockstar/package');
 /**
  *
  * @param {Object} args
- * @param {String} args.mockstar mockstar 的版本号 输入 --mockstar=1.5.0
+ * @param {Boolean} [args.dev] 是否为开发者模式，使用方式: --dev
+ * @param {String} [args.mockstar] mockstar 的版本号 输入 --mockstar=1.5.0
  */
 module.exports = function (args) {
     // node ./bin/mockstar init --mockstar=1.2.3
@@ -28,7 +29,8 @@ module.exports = function (args) {
 
     // 可以通过透传额外参数到 generator 中，然后通过 this.options 就能够取到传递过去的值
     let yeoResult = yeomanEnv.run(name, {
-        'pkgVersion': pkgVersion
+        'pkgVersion': pkgVersion,
+        'isDev': args.dev
     }, err => {
         // console.log('=====end===', err);
         // 这里的 yeoResult 即 generator 的 this 对象，例如可以通过 result.destinationPath() 获得生成的地址
