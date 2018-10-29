39afd5bfc568cb0534d62cb9d9e59e384acd2e76
helinjiang <onehlj@qq.com>
2018-10-29 22:51:53 +0800

    refactor: stop 时的默认名字在命令中

--- packages/mockstar-cli/libs/internal/stop/stop.js
+++ packages/mockstar-cli/libs/internal/stop/stop.js
@@ -10,14 +10,14 @@ const localServer = require('../../../biz/local-server');
  * @param {Array} [args._] 参数数组，取第一个值为 pm2 服务的名称
  */
 module.exports = function (args) {
-    console.log(args._[0]);
+    // console.log(args._[0]);
 
     // 自定义的pm2服务名称
     // mockstar stop mockstar_9527
-    const name = args._[0];
+    const name = args._[0] || 'mockstar_9527';
 
     // 停止本地服务
-    console.log('Ready to stop local server of ' + name);
+    console.log('Ready to stop local server which app_name = ' + name);
     localServer.stopServer(name);
 
     return Promise.resolve();
