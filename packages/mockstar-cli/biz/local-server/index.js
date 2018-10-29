f10978138ec5e5d2031075b88884320aa2f3c5e5
linjianghe <linjianghe@tencent.com>
2018-10-29 22:47:38 +0800

    refactor: 支持启动不同的端口和pm2应用名字

--- packages/mockstar-cli/biz/local-server/index.js
+++ packages/mockstar-cli/biz/local-server/index.js
@@ -1,41 +1,22 @@
 const run = require('./run');
 const runByPm2 = require('./run-by-pm2');
 
-const runConfig = require('./config');
-
 /**
  * 启动服务
  *
- * @param {Boolean} isDev 是否为开发者模式
- * @param {String} configAbsolutePath mockstar.config.js 文件的绝对路径
- * @param {String} [cwd] 项目启动的目录，默认为 process.cwd()
- * @param {Object} [opts] 额外的高优先级的参数，用于覆盖 mockstarConfig 的值
- * @param {Number} [opts.port] 自定义服务启动端口
- * @param {String} [opts.name] 自定义的pm2服务名称
+ * @param {Object} configOpts mockstar.config.js中的配置项
+ * @param {String} [configOpts.rootPath] 项目根目录
+ * @param {String} [configOpts.buildPath] 构建之后的目录
+ * @param {String} [configOpts.logPath] 日志目录
+ * @param {String} [configOpts.mockServerPath]  mock server 根目录
+ * @param {Number} [configOpts.port] 端口号
+ * @param {String} [configOpts.name] pm2 应用的名字
+ * @param {Boolean} [configOpts.isDev] 当前是否为开发模式，即不启用pm2
  */
-function startServer(isDev, configAbsolutePath, cwd, opts) {
-    // 开发模式下，直接调用执行
-    let mockstarConfig = require(configAbsolutePath);
+function startServer(configOpts = {}) {
+    // console.log('--startServer--', configOpts);
 
-    // 如果不定义 rootPath，则默认取 process.cwd()
-    if (!mockstarConfig.rootPath) {
-        mockstarConfig.rootPath = cwd || process.cwd();
-    }
-
-    // 获取一些默认值
-    let configOpts = runConfig.getConfigOpts(mockstarConfig);
-
-    // 如果没法获取配置项，则将无法启动成功
-    if (!configOpts) {
-        throw new Error('Invalid param!');
-    }
-
-    // 从该处传入的参数优先级最高，一般用于命令行传入
-    if (opts) {
-        configOpts = Object.assign({}, configOpts, opts);
-    }
-
-    if (isDev) {
+    if (configOpts.isDev) {
         run(configOpts);
     } else {
         runByPm2.start(configOpts);
