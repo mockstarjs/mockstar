4b7eb5ebd8bd13104ee2600af195b29759524a09
helinjiang <onehlj@qq.com>
2018-10-29 19:42:41 +0800

    fix: 修复mockstar start --port=xxxx 无效 #19

--- packages/mockstar-cli/biz/local-server/start-app.js
+++ packages/mockstar-cli/biz/local-server/start-app.js
@@ -1,26 +1,13 @@
 const run = require('./run');
-const runConfig = require('./config');
 
-const configAbsolutePath = process.argv[2];
-const cwd = process.argv[3];
+// console.log('==', process.argv);
 
-console.log('configAbsolutePath:', configAbsolutePath);
-console.log('cwd:', cwd);
+let configOpts;
 
-// 获取配置文件内容
-let mockstarConfig = require(configAbsolutePath);
-
-// 如果不定义 rootPath，则默认取 process.cwd()
-if (!mockstarConfig.rootPath) {
-    mockstarConfig.rootPath = cwd || process.cwd();
-}
-
-// 获取一些默认值
-let configOpts = runConfig.getConfigOpts(mockstarConfig);
-
-// 如果没法获取配置项，则将无法启动成功
-if (!configOpts) {
-    throw new Error('Invalid param!');
+try {
+    configOpts = JSON.parse(process.argv[2]);
+} catch (e) {
+    throw e;
 }
 
 run(configOpts);
