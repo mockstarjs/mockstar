bd3dc4b7c3ee90bababdbc87649e335e3b42037c
helinjiang <onehlj@qq.com>
2018-11-09 18:03:23 +0800

    feat: 暴露 MockStarQuery 出来

--- packages/mockstar/src/index.js
+++ packages/mockstar/src/index.js
@@ -1,10 +1,12 @@
 import Mocker from './model/Mocker';
 import Parser from './model/Parser';
+import MockStarQuery from './model/MockStarQuery';
 
 import { getQueryItem } from './util';
 
 module.exports = {
     Mocker: Mocker,
     Parser: Parser,
+    MockStarQuery: MockStarQuery,
     getQueryItem: getQueryItem
 };
