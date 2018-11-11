fb0195e207adbd324c5a9cde37ea0394a8541d70
linjianghe <linjianghe@tencent.com>
2018-11-11 12:00:23 +0800

    feat: 新增一个demo

---
+++ packages/mockstar-cli/test/data/production-server-test/mockstar.config.js
@@ -0,0 +1,7 @@
+const path = require('path');
+
+module.exports = {
+    rootPath: __dirname,
+    mockServerPath: path.resolve(__dirname, '../../../../mockstar/test/data/fixtures/mock_server/mockers'),
+    namespace: 'now_qq_com/linjianghe'
+};
