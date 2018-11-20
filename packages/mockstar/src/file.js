fbd01dee06bf818fbddcc2cd663075089d4f598a
linjianghe <linjianghe@tencent.com>
2018-11-20 18:48:21 +0800

    feat: 如果文件变化了，则重启

--- packages/mockstar/src/file.js
+++ packages/mockstar/src/file.js
@@ -1,4 +1,34 @@
-export function getRequireResult(absolutePath) {
-    // TODO 此处要考虑下是否安全
-    return require(absolutePath);
+/**
+ * 加载模块
+ *
+ * @param {String} moduleId 模块，绝对路径
+ * @param {Boolean} [ignoreCache] 是否忽略缓存
+ * @return {*}
+ */
+export function requireModule(moduleId, ignoreCache) {
+    if (typeof moduleId !== 'string') {
+        throw new TypeError('Expected a string');
+    }
+
+    const filePath = require.resolve(moduleId);
+    ignoreCache = true;
+    // 忽略缓存
+    if (ignoreCache) {
+        // Delete itself from module parent
+        if (require.cache[filePath] && require.cache[filePath].parent) {
+            let i = require.cache[filePath].parent.children.length;
+
+            while (i--) {
+                if (require.cache[filePath].parent.children[i].id === filePath) {
+                    require.cache[filePath].parent.children.splice(i, 1);
+                }
+            }
+        }
+
+        // Delete module from cache
+        delete require.cache[filePath];
+    }
+
+    // Return fresh module
+    return require(filePath);
 }
