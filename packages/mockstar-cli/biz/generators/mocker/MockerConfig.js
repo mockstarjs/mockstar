87012c881b2ab267549598369124955e5bec6d21
helinjiang <onehlj@qq.com>
2018-11-21 22:48:27 +0800

    feat: 通过输入 CGI 请求自动生成mocker

--- packages/mockstar-cli/biz/generators/mocker/MockerConfig.js
+++ packages/mockstar-cli/biz/generators/mocker/MockerConfig.js
@@ -1,11 +1,25 @@
+const urlParse = require('url-parse');
+
 module.exports = class MockerConfig {
     constructor() {
         this.mockerParentPath = '';
         this.mockerName = '';
+        this.route = '';
+        this.host = '';
+        this.method = 'get';
     }
 
     updateByAnswer(opts) {
         this.mockerParentPath = opts.mockerParentPath;
         this.mockerName = opts.mockerName;
+        this.method = opts.method;
+
+        const urlParseResult = urlParse(opts.reqURL);
+
+        this.route = urlParseResult.pathname;
+        this.host = urlParseResult.host;
     }
 };
+
+// console.log(urlParse('http://now.qq.com/index.html?a=111'))
+// console.log(urlParse('/cgi-bin/index.html?a=111'))
