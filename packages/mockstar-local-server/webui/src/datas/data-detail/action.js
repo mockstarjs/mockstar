d65301eab76cf4bc95a016ce0f33c705f62a2cce
linjianghe <linjianghe@tencent.com>
2018-11-21 19:46:25 +0800

    feat: 调用 CGI 来获取当前服务的状态

---
+++ packages/mockstar-local-server/webui/src/datas/data-detail/action.js
@@ -0,0 +1,21 @@
+import { CALL_API } from '../../middlewares/api';
+import { getCGIBase } from '../../custom';
+
+export const DETAIL_REQUEST = 'DETAIL_REQUEST';
+export const DETAIL_REQUEST_SUCCESS = 'DETAIL_REQUEST_SUCCESS';
+export const DETAIL_REQUEST_FAIL = 'DETAIL_REQUEST_FAIL';
+
+function fetchDetail() {
+    return {
+        [CALL_API]: {
+            types: [DETAIL_REQUEST, DETAIL_REQUEST_SUCCESS, DETAIL_REQUEST_FAIL],
+            url: `${getCGIBase()}/detail`
+        }
+    };
+}
+
+export function loadDetail() {
+    return (dispatch, getState) => {
+        return dispatch(fetchDetail());
+    };
+}
