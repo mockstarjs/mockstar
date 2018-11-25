64c34bd6bdd7e96732014fe98e3c7ce8256d3530
helinjiang <onehlj@qq.com>
2018-11-25 18:54:43 +0800

    feat: get和post还是有些差异的，希望管理页面能够做点明显区别 #42

--- packages/mockstar-local-server/webui/src/pages/mockers/components/mocker/display-menu/index.js
+++ packages/mockstar-local-server/webui/src/pages/mockers/components/mocker/display-menu/index.js
@@ -5,50 +5,51 @@ import { withRouter } from 'react-router-dom';
 import './index.less';
 
 class MockerMenu extends Component {
-  constructor(props, context) {
-    super(props, context);
+    constructor(props, context) {
+        super(props, context);
 
-  }
-
-  handleClick = ({ item, key }) => {
-    if (this.props.match.params.mockerName !== key) {
-      this.props.history.push(this.props.match.path.replace(/:mockerName/gi, key));
-
-      // 通知右侧重新刷新
-      this.props.refresh(key);
-    }
-  };
-
-  render() {
-    let { mockerListInfo, match } = this.props;
-    if (!mockerListInfo.isLoaded) {
-      return null;
     }
 
-    let { mockerName } = match.params;
+    handleClick = ({ item, key }) => {
+        if (this.props.match.params.mockerName !== key) {
+            this.props.history.push(this.props.match.path.replace(/:mockerName/gi, key));
 
-    return (
-      <div className="mocker-menu">
-        <Menu
-          defaultSelectedKeys={[mockerName]}
-          selectedKeys={[mockerName]}
-          onClick={this.handleClick}
-        >
+            // 通知右侧重新刷新
+            this.props.refresh(key);
+        }
+    };
 
-          {
-            mockerListInfo.list.map((item) => {
-              return (
-                <Menu.Item key={item.name}>
-                  {item.name}
-                </Menu.Item>
-              );
-            })
-          }
+    render() {
+        let { mockerListInfo, match } = this.props;
+        if (!mockerListInfo.isLoaded) {
+            return null;
+        }
 
-        </Menu>
-      </div>
-    );
-  }
+        let { mockerName } = match.params;
+
+        return (
+            <div className="mocker-menu">
+                <Menu
+                    defaultSelectedKeys={[mockerName]}
+                    selectedKeys={[mockerName]}
+                    // style={{ width: '300px' }}
+                    onClick={this.handleClick}
+                >
+
+                    {
+                        mockerListInfo.list.map((item) => {
+                            return (
+                                <Menu.Item key={item.name} className={item.config.plugin + ' ' + item.config.method}>
+                                    {item.name}
+                                </Menu.Item>
+                            );
+                        })
+                    }
+
+                </Menu>
+            </div>
+        );
+    }
 }
 
 export default withRouter(MockerMenu);
