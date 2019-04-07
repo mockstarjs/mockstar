d6876c86ec9eef015a7e422fbe57317d7004b34e
helinjiang <me@helinjiang.com>
2019-04-08 06:59:48 +0800

    refactor: 将一些常量移动到 config.js 中统一维护

--- packages/mockstar/src/index.js
+++ packages/mockstar/src/index.js
@@ -3,7 +3,7 @@ import Parser from './model/Parser';
 import MockStarQuery from './model/MockStarQuery';
 
 import { getQueryItem } from './util';
-import { MS_DISABLE, MS_QUERY_KEY } from './config';
+import { MS_DISABLE, MS_QUERY_KEY, MS_FROM } from './config';
 
 module.exports = {
     Mocker,
@@ -11,5 +11,6 @@ module.exports = {
     MockStarQuery,
     getQueryItem,
     MS_DISABLE,
-    MS_QUERY_KEY
+    MS_QUERY_KEY,
+    MS_FROM
 };
