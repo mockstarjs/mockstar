21a9f93b04e50e3cd1dcad0aa25b3fbaab71f386
linjianghe <linjianghe@tencent.com>
2019-04-08 17:07:59 +0800

    feat: 补充测试用例

--- packages/mockstar/test/data/fixtures/mock_server/mockers/demo_02/mock_modules/success_4/index.js
+++ packages/mockstar/test/data/fixtures/mock_server/mockers/demo_02/mock_modules/success_4/index.js
@@ -3,5 +3,9 @@ module.exports = function (params) {
         return 'from_param_' + params.a;
     }
 
+    if (params && params._ms_extra) {
+        return '_ms_extra_' + (typeof params._ms_extra === 'object' ? JSON.stringify(params._ms_extra) : params._ms_extra);
+    }
+
     return 4;
 };
