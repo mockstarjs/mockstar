47fe2325d3ee6ea4cff4f3523702415d035a44b9
helinjiang <onehlj@qq.com>
2020-08-27 18:24:41 +0800

    feat: 补充单元测试

--- packages/mockstar-generators/test/scripts/project/index.test.ts
+++ packages/mockstar-generators/test/scripts/project/index.test.ts
@@ -63,6 +63,49 @@ describe('./project/index.ts', () => {
     });
   });
 
+  describe('initProject(mockstar-app-9420)', () => {
+    const name = 'mockstar-app-9420';
+    const fixtureSaveDir = path.join(FIXTURE_PATH, name);
+    const tmpSaveDir = path.join(TMP_PATH, name);
+
+    before(async () => {
+      await initProject({
+        isDev: false,
+        parentPath: TMP_PATH,
+        name: name,
+        port: 9420,
+      });
+    });
+
+    after(() => {
+      fse.removeSync(tmpSaveDir);
+    });
+
+    it('check all files exits', async () => {
+      const tmpPaths = walkSync(tmpSaveDir);
+      const expectPaths = walkSync(fixtureSaveDir);
+
+      expect(tmpPaths).to.eql(expectPaths);
+      expect(tmpPaths.length).to.equal(27);
+    });
+
+    it('check all files equal', async () => {
+      const tmpPaths = walkSync.entries(tmpSaveDir);
+
+      tmpPaths.forEach(entry => {
+        if (!entry.isDirectory()) {
+          const tmpContent = fse.readFileSync(path.join(tmpSaveDir, entry.relativePath), 'utf8');
+          const expectContent = fse.readFileSync(
+            path.join(fixtureSaveDir, entry.relativePath),
+            'utf8',
+          );
+
+          expect(tmpContent).to.equal(expectContent);
+        }
+      });
+    });
+  });
+
   describe('getProjectGeneratorTemplatesRoot()', () => {
     it('getProjectGeneratorTemplatesRoot should be a function', () => {
       expect(getProjectGeneratorTemplatesRoot).to.be.an('function');
